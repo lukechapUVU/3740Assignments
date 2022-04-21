8e5caff5610a573cbca978196b8c99592ce2c2e6
Luke <10928675@uvu.edu>
2022-04-20 20:05:21 -0600

    added buddy customer comments

--- phpsite/buddy.php
+++ phpsite/buddy.php
@@ -10,11 +10,9 @@
         or my name isn't Buddy Sorrell. I've been in the business for over 30 years. I put so much work into 
         my clients that I have never met my three children and my wife left me years ago. All of that doesn't 
         matter though. What does matter is that you get on a big party boat for a low cost!</p>
-    <p><strong>Customer Comment: </strong><br> "Sally is very helpful. She was super friendly and helped me and
-        my wife find the best cruise package that we could afford for our honeymoon. We get a Christmas card from 
-        Sally every year just because she's so whole-hearted and loves her customers." - Tim Garth </p>
-    <br> "Mrs Rogers is the best agent I could have asked for. I wish that she worked in real estate
-        too because maybe I could have gotten a better rate on my mortgage! She showed me her toy boat that
-        she got as a child. I can't believe she still holds onto that thing!" - Veronica Withers
+    <p><strong>Customer Comment: </strong><br> "Buddy gave me a great price. I tried three other travel 
+        agents before working with Buddy, and I have to say, his time in the business surely shows." - Joe Schmoe </p>
+    <br> "If you're looking for someone who will treat you how they would want to be treated then you should
+        your business with Buddy. He's very considerate towards his clients and always wants the best for them" - Kallie Knight
 </div>
 <?php include '_bot.php';?>
